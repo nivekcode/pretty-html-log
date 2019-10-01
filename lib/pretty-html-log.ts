2e9d91708390d6c8f29ab73bcaeff1ee8cebfa93
kreuzerk <kevin.kreuzer90@icloud.com>
2019-10-01 10:13:44 +0200

    feat(prettier): use prettier to parseHTML

--- lib/pretty-html-log.ts
+++ lib/pretty-html-log.ts
@@ -1,5 +1,8 @@
 import {HighlightEngine} from './core/highlight-engine';
+import * as prettier from 'prettier';
 
 const highlightEngine = new HighlightEngine();
 
-export const highlight = (value) => highlightEngine.highlight(value);
+export const highlight = (value) => highlightEngine.highlight(prettier.format(value, {
+    parser: 'html'
+}));
