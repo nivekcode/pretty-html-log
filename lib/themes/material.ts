9d8b0cf498a20dd8e5e489d82556acc74f3339a7
kreuzerk <kevin.kreuzer90@icloud.com>
2019-10-02 21:41:42 +0200

    feat(theme): add vscode and material theme

---
+++ lib/themes/material.ts
@@ -0,0 +1,12 @@
+import {Theme} from './theme.model';
+
+export const DRACULA_THEME: Theme = {
+    doctag: "#546e7a",
+    meta: '#7f7f7f',
+    comment: '#546e7a',
+    tag: '#ff5370',
+    attr: '#ffcb6b',
+    string: '#c3e88d',
+    name: '#ff5370',
+    tagContent: '#ffffff'
+};
