5a6e9d3e4467706ac6fafbeae9bd56d7593a47cf
kreuzerk <kevin.kreuzer90@icloud.com>
2019-10-11 09:13:00 +0200

    test(logger): fix failing logger test

--- src/__mocks__/chalk.ts
+++ src/__mocks__/chalk.ts
@@ -3,4 +3,8 @@ const chalk = jest.genMockFromModule<any>('chalk');
 chalk.hex = (hexcode: string) => (value: string) =>
   value === '<' ? 'Opening tag' + value : value;
 
+chalk.bgCyan = {
+  black: jest.fn()
+};
+
 module.exports = chalk;
